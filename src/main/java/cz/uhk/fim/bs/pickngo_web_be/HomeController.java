7109aff4dd7bdf98302c7a99591e836d2dd07000
Štěpán Cellar <stepan.cellar@gmail.com>
2021-04-21 21:19:32 +0200

    pripojeni na cloud db

---
+++ src/main/java/cz/uhk/fim/bs/pickngo_web_be/HomeController.java
@@ -0,0 +1,18 @@
+package cz.uhk.fim.bs.pickngo_web_be;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping(path="/")
+public class HomeController {
+
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String getHello(){
+        return "hello there";
+    }
+}
